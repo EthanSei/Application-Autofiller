e7dabc4d667f759e766f75774f484ec7caf64f72
Ethan Seide <eseide@princeton.edu>
2019-09-04 13:42:04 -0400

    Initial Commit. Base extension
    First commit

---
+++ leverContent.js
@@ -0,0 +1,15 @@
+console.log("Loaded Content Script");
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log("Message received");
+    if (request.message = "data_sent") {
+        var fields = request.fields;
+        var inputs = document.getElementsByTagName("INPUT");
+
+        for (var i = 0; i < inputs.length; i++) {
+            if (inputs[i].name == "name") inputs[i].value = fields.firstName + " " + fields.lastName;
+            else if (inputs[i].name == "email") inputs[i].value = fields.email;
+            else if (inputs[i].name == "phone") inputs[i].value = fields.phone;
+        }
+    }
+});
---
+++ popup.js
@@ -0,0 +1,120 @@
+
+document.getElementById("submit").addEventListener("click", function(event) {
+    // Save form data
+    var fields = {
+        firstName : document.getElementById("first_name").value,
+        lastName : document.getElementById("last_name").value,
+        email : document.getElementById("email").value,
+        phone : document.getElementById("phone").value,
+        location : document.getElementById("location").value,
+        linkedin : document.getElementById("linkedin").value,
+        website : document.getElementById("website").value,
+        gender : document.getElementById("gender").value
+    }
+    chrome.storage.local.set({"firstName": fields.firstName,
+                                "lastName": fields.lastName,
+                                "email": fields.email,
+                                "phone": fields.phone,
+                                "location": fields.location,
+                                "linkedin": fields.linkedin,
+                                "website": fields.website,
+                                "gender": fields.gender
+                            }, function() { alert("Saving: " + JSON.stringify(fields)); }
+    );
+
+    chrome.tabs.query({active : true, currentWindow: true}, function(tabs) {
+        var activeTab = tabs[0];
+        console.log("Sending message: " + JSON.stringify(fields));
+        chrome.tabs.sendMessage(activeTab.id, {"message": "data_sent", "fields": fields}, function() {
+            console.log("Message sent");
+        });
+    });
+});
+
+var fieldNames = ['firstName','lastName','email','phone','location','linkedin','website','gender'];
+
+chrome.storage.local.get(fieldNames, (fields) => {
+    console.log("GETTING FIELDS");
+    if (fields != null) {
+        // alert("LOADED FIELDS");
+        document.getElementById("first_name").value = fields['firstName'];
+        document.getElementById("last_name").value = fields['lastName'];
+        document.getElementById("email").value = fields['email'];
+        document.getElementById("phone").value = fields['phone'];
+        document.getElementById("location").value = fields['location'];
+        document.getElementById("linkedin").value = fields['linkedin'];
+        document.getElementById("website").value = fields['website'];
+        document.getElementById("gender").value = fields['gender'];
+    }
+    else {
+        // alert("NULL FIELDS");
+        console.log("Couldn't Load Fields");
+    }
+});
+
+
+
+// $(document).ready(function() {
+//     console.log("Loaded Jquery!");
+//     var fields = ['first_name','last_name','email','phone','location','linkedin','website','gender'];
+//     $("#save").on('click', function() {
+//         console.log("Clicked!");
+//         saveFormData();
+//         getFormData(fields, function(items) {
+//             if (items != null) {
+//                 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//                     var activeTab = tabs[0];
+//                     console.log("Sending message..." + JSON.stringify(items));
+//                     chrome.tabs.sendMessage(activeTab.id, {"message": "data_sent","items":items});
+//                 });
+//             }
+//             else {
+//                 console.log("ERROR: Items returned null");
+//             }
+//         });
+//     });
+//     getFormData(fields, function(items) {
+//         if (items != null) {
+//             $("#first_name").val(items['first_name']);
+//             $("#last_name").val(items['last_name']);
+//             $("#email").val(items['email']);
+//             $("#phone").val(items['phone']);
+//             $("#location").val(items['location']);
+//             $("#linkedin").val(items['linkedin']);
+//             $("#website").val(items['website']);
+//             $("#gender").val(items['gender']);
+
+//             console.log("Assigning values: " + $('#first_name').val());
+//         }
+//         else {
+//             console.log("Init values not specified");
+//         }
+//     });
+// });
+// function saveFormData() {
+//     var $fname = $("#first_name").val();
+//     var $lname = $("#last_name").val();
+//     var $email = $("#email").val();
+//     var $phone = $("#phone").val();
+//     var $location = $("#location").val();
+//     var $linkedin = $("#linkedin").val();
+//     var $website = $("#website").val();
+//     var $gender = $("#gender").val();
+
+//     if (!$fname || !$lname) {
+//         console.log("ERROR: Data not specified in required fields!");
+//         return;
+//     }
+
+//     chrome.storage.sync.set({'first_name':$fname, 'last_name':$lname, 'email':$email, 'phone':$phone, 'location':$location, 'linkedin':$linkedin, 'website':$website, 'gender':$gender}, function() {
+//         console.log("Settings saved!");
+//     });
+
+
+// }
+// function getFormData(key, callback) {
+//     chrome.storage.sync.get(key, (items) => {
+//         console.log('Returned: ' + items['name']);
+//         callback(items);
+//     });
+// }
